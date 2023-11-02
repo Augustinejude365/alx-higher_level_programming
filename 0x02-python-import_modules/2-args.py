059dd398c5b8b31e3827f58659946133373bccd9
Augustinejude365 <divinefavour4jude@gmail.com>
2023-11-02 16:30:28 -0700

    imported

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys
     argjoe = sys.argv
     sizebee = len(argjoe) - 1
-
     if sizebee > 1:
         print("{} arguments:".format(sizebee))
         for bat in range(1, sizebee + 1):
             print("{}: {}".format(bat, argjoe[bat]))
-
-        elif sizebee = 0:
+        elif sizebee == 0:
             print("{} arguments.".format(sizebee))
-
         else:
             print("{} argument:".format(sizebee))
             print("{}: {}".format(sizebee, argjoe[1]))
