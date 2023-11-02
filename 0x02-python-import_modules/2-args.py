21fefb9f0a831a8aea18a46542bebaa75a5cafb0
Augustinejude365 <divinefavour4jude@gmail.com>
2023-11-02 14:58:20 -0700

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
     argJoe = len(sys.argv)
     if argJoe == 1:
-        print("{} arguments:".format(argJoe - 1))
+        print("{} arguments.".format(argJoe - 1))
     elif argJoe == 2:
-        print("{} arguments:".format(argJoe - 1))
+        print("{} argument:".format(argJoe - 1))
     else:
         print("{} arguments:".format(argJoe - 1))
-        for b in range(1, argJoe):
-            print("{}: {}".format(b, sys.argv[b]))
+        for bat in range(1, argJoe):
+            print("{}: {}".format(bat, sys.argv[bat]))
