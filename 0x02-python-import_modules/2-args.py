f4423e07299405217736297713a0d64d32eac381
Augustinejude365 <divinefavour4jude@gmail.com>
2023-11-02 17:35:34 -0700

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,9 +2,9 @@
 if __name__ == "__main__":
     import sys
     kountag = len(sys.argv) - 1
-    if kountag == 0:
-        print("0 argument.")
-    elif kountag == 1:
+    if kountag == 1:
+        print("1 argument.")
+    elif kountag < 1:
         print("1 argument:")
     else:
         print("{} arguments:".format(kountag))
