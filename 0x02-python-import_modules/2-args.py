329d027392edd9a48ece3e9f7799ccc56528332f
Augustinejude365 <divinefavour4jude@gmail.com>
2023-11-02 21:14:08 -0700

    uppercase ascii

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys
+
     kountag = len(sys.argv) - 1
-    if kountag == 1:
-        print("1 argument.")
-    elif kountag < 1:
+    if kountag == 0:
+        print("0 arguments.")
+    elif kountag == 1:
         print("1 argument:")
     else:
         print("{} arguments:".format(kountag))
