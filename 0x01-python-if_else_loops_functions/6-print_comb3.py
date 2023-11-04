1cbb6bab6cff55cf5308d3e3a95efb49e6060be5
Augustinejude365 <divinefavour4jude@gmail.com>
2023-11-04 15:57:34 -0700

    updated

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for j in range(0, 10):
+    for k in range(1, 10):
+        if j >= k:
+            continue
+        elif j == 8 and k == 9:
+            print("{}{}".format(j,k))
+        else:
+            print"{}{}".format(j,k), end=", ")
