a710f95178fbfa38c2f770e1b250857d3aef5ff6
Augustinejude365 <divinefavour4jude@gmail.com>
2023-11-06 15:49:28 -0800

    Tuples addition

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ""
-    for my_elements in my_string:
-        if my_elements != "c" and my_elements != "C":
-            new_string += my_elements
-            return new_string
+    new_str = ""
+    for i in my_string:
+        if (i != 'c') and (i != 'C'):
+            new_str += i
+            return (new_str)
