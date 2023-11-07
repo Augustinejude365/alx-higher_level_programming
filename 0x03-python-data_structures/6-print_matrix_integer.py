14bf059d0181456f984186899407700f0e248d41
Augustinejude365 <divinefavour4jude@gmail.com>
2023-11-06 16:02:07 -0800

    list of matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        print()
-    else:
-        for row_x in range(len(matrix)):
-            for var in range(len(matrix[row_x])):
-                if var != len(matrix[row_x]) - 1:
-                    colum_x = ' '
-                else:
-                    colum_xe = ''
-                    print("{:d}".format(matrix[row_x][var]), end=colum_x)
-                    print()
+    for k in matrix:
+        print(' '.join('{:d}'.format(j)for j in k))
