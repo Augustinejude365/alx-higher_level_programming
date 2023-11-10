60ff77e953f29f19fdce1dfb67a75cc19f6181af
Augustinejude365 <divinefavour4jude@gmail.com>
2023-11-10 13:10:02 -0800

    arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     from sys import argv
     kount = 0
     for g in range(1, len(argv)):
-        kount += int(argv[g])
-        print("{}".format(kount))
+    kount += int(argv[g])
+    print("{}".format(kount))
