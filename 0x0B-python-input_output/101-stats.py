5df51f7ee79bef4d06220e78df9eaa0290308aa5
Augustinejude <divinefavour4jude@gmail.com>
2023-12-05 16:03:42 -0800

    Python - Input/Output update 2

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -33,13 +33,13 @@ def print_stats(size, status_codes):
                     else:
                         count += 1
                         line = line.split()
-            try:
-                size += int(line[-1])
-            except (IndexError, ValueError):
-                pass
-            try:
-                if line[-2] in valid_codes:
-                    if status_codes.get(line[-2], -1) == -1:
+                        try:
+                            size += int(line[-1])
+                        except (IndexError, ValueError):
+                            pass
+                        try:
+                            if line[-2] in valid_codes:
+        if status_codes.get(line[-2], -1) == -1:
                         status_codes[line[-2]] = 1
                     else:
                         status_codes[line[-2]] += 1
