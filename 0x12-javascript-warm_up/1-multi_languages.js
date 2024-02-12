fc36c0eef32f9e46eae6619df10e82a2a26c3150
AugustineJude <divinefavour4jude@gmail.com>
2024-02-12 09:16:04 -0800

    javascript warm up 0 to 2

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// a script that prints 3 lines
+
+// create constant variables, each with the desired message output.
+const line1 = 'C is fun';
+const line2 = 'Python is cool';
+const line3 = 'JavaScript is amazing';
+
+/* 
+The console.log() function is used three times to print each line to the console, one at a time.
+*/
+
+console.log(line1);
+console.log(line2);
+console.log(line3);
