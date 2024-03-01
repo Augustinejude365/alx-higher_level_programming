7f61df16186eb4f9d1b892431660ac5c5b4012e5
AugustineJude <divinefavour4jude@gmail.com>
2024-03-01 13:26:54 -0800

    error code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,15 +3,24 @@
 a Python script that takes in a URL, sends a request to the URL and
 displays the body of the response (decoded in utf-8).
 """
-from urllib import request, parse
-import sys
+from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
+from sys import argv
+
+
+def error_code():
+    """
+    This sends a request to the URL and displays the body of the
+    response
+    """
+    url = argv[1]
+    try:
+        with urlopen(url) as response:
+            the_page = response.read()
+            print(the_page.decode('utf-8'))
+    except HTTPError as e:
+        print('Error code: {}'.format(e.code))
 
 
 if __name__ == "__main__":
-    values = {'email': sys.argv[2]}
-    data = parse.urlencode(values)
-    data = data.encode('ascii')
-    req = request.Request(sys.argv[1], data)
-    with request.urlopen(req) as response:
-        body = response.read()
-        print(body.decode('utf-8'))
+    error_code()
