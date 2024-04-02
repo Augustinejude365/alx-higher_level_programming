de223b0dfc0f9d6589938ccd16ae6612208032c5
Augustinejude365 <divinefavour4jude@gmail.com>
2024-04-02 13:30:15 -0700

    To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for kase in str:
-        if ord("a") <= ord(kase) <= ord("z"):
-            kase = chr(ord(kase) - 32)
-            print("{:s}".format(kase), end="")
-            print()
+    for i in str:
+        if ord(i) in range(97, 123):
+            i = chr(ord(i) - 32)
+        print("{:s}".format(i), end="")
+    print()
