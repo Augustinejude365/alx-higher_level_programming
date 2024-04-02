b9b5e9cce58ed983b2f90ddfa019bda0362133e7
Augustinejude365 <divinefavour4jude@gmail.com>
2024-04-02 14:14:28 -0700

    Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,56 +1,48 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <stddef.h>
 
 /**
- * add_nodeint - A function that adds a new node at the beginning
- * of a listint_t list
- * @head: pointer to the head of listint_t
- * @n: int to add in listint_t list
- * Return: address of the new element, or NULL if it failed
- */
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-	listint_t *new;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->next = *head;
-	*head = new;
-	return (new);
-}
-/**
- * is_palindrome - A function that identifies if a syngle linked
- * list is palindrome
- * @head: pointer to the head of listint_t
- * Return: 1 if it is palindrome else 0
+ * is_palindrome - tests if linked lists is palindrome
+ * @head: address of pointer to list
+ * Return: 1 is palindrome else 0
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *head2 = *head;
-	listint_t *aux = NULL, *aux2 = NULL;
+	listint_t *slow = *head, *fast = *head, *node, *prev;
+	int failed = 0;
 
-	if (*head == NULL || head2->next == NULL)
-		return (1);
-	while (head2 != NULL)
+	while (fast != NULL && fast->next != NULL)
 	{
-		add_nodeint(&aux, head2->n);
-		head2 = head2->next;
+		fast = fast->next->next;
+		slow = slow->next;
 	}
-	aux2 = aux;
-	while (*head != NULL)
+	node = slow;
+	prev = NULL;
+	while (node)
 	{
-		if ((*head)->n != aux2->n)
+		fast = node->next;
+		node->next = prev;
+		prev = node;
+		node = fast;
+	}
+	fast = *head;
+	node = prev;
+	while (prev)
+	{
+		if (fast->n != prev->n)
 		{
-			free_listint(aux);
-			return (0);
+			failed = 1;
+			break;
 		}
-		*head = (*head)->next;
-		aux2 = aux2->next;
+		fast = fast->next;
+		prev = prev->next;
 	}
-	free_listint(aux);
-	return (1);
+	prev = NULL;
+	while (node)
+	{
+		fast = node->next;
+		node->next = prev;
+		prev = node;
+		node = fast;
+	}
+	return (!failed);
 }
