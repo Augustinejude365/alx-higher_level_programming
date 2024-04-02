f188d20fae7a4429e3fbf6f8ec27bbdaed207061
Augustinejude365 <divinefavour4jude@gmail.com>
2024-04-02 13:54:24 -0700

    Can you C me now?

--- 0x03-python-data_structures/100-test_lists.py
+++ 0x03-python-data_structures/100-test_lists.py
@@ -2,20 +2,20 @@ import ctypes
 
 lib = ctypes.CDLL('./libPyList.so')
 lib.print_python_list_info.argtypes = [ctypes.py_object]
-l = ['hello', 'World']
-lib.print_python_list_info(l)
-del l[1]
-lib.print_python_list_info(l)
-l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], "My string"]
-lib.print_python_list_info(l)
-l = []
-lib.print_python_list_info(l)
-l.append(0)
-lib.print_python_list_info(l)
-l.append(1)
-l.append(2)
-l.append(3)
-l.append(4)
-lib.print_python_list_info(l)
-l.pop()
-lib.print_python_list_info(l)
+num_list = ['hello', 'World']
+lib.print_python_list_info(num_list)
+del num_list[1]
+lib.print_python_list_info(num_list)
+num_list = num_list + [4, 5, 6.0, (9, 8), [9, 8, 1024], "My string"]
+lib.print_python_list_info(num_list)
+num_list = []
+lib.print_python_list_info(num_list)
+num_list.append(0)
+lib.print_python_list_info(num_list)
+num_list.append(1)
+num_list.append(2)
+num_list.append(3)
+num_list.append(4)
+lib.print_python_list_info(num_list)
+num_list.pop()
+lib.print_python_list_info(num_list)
